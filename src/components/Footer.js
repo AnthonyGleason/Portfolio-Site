d4e1ca2bf8a88ede7e41a10a53f89f65633a924d
Anthony Infortunio <contact@anthonyinfortun.io>
2023-04-15 19:25:57 -0400

    .

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,10 @@ export default function Footer(){
   return(
     <div className='footer'>
       <h1 className='footer-title'>Contact Me</h1>
-      <p className='footer-text'>I look forward to reading your message!</p>
+      <p className='footer-text'>
+        I look forward to reading your message!<br /> <br />
+        <em className='em'>contact@anthonyinfortun.io</em>
+      </p>
       <img className='arrow' src={arrow} alt='arrow that loops once and points to the right' />
     </div>
   )
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import '../styles/intro.css';
+import me from '../assets/me.jpg';
 export default function Intro(){
   return(
     <div className='intro'>
-      <h1 className='name'>
-        Anthony <br />
-        Infortunio
-      </h1>
-      <h3 className='title'>Full-Stack Web Developer</h3>
+      <img src={me} className='intro-img' />
+      <div className='intro-content'>
+        <h1 className='name'>
+          Anthony <br />
+          Infortunio
+        </h1>
+        <h3 className='title'>Full-Stack Web Developer</h3>
+      </div>
     </div>
   )
 }
