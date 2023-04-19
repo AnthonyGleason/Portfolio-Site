d77a7aa85a23734160e30929eeff9f2e2c06cd83
Anthony Infortunio <contact@anthonyinfortun.io>
2023-04-19 18:18:41 -0400

    add resume, add skills section margin

--- src/components/ContactButtons.js
+++ src/components/ContactButtons.js
@@ -3,6 +3,7 @@ import githubLogo from '../assets/github.svg';
 import linkedinLogo from '../assets/linkedin.svg';
 import mailImg from '../assets/mail.svg';
 import resumeImg from '../assets/resume.svg';
+import resumePDF from '../assets/resume.pdf';
 import '../styles/contact-buttons.css';
 export default function ContactButtons(){
   return(
@@ -10,7 +11,7 @@ export default function ContactButtons(){
       <img className='expand' onClick={()=>{window.location.href='https://www.github.com/antinf'}} src={githubLogo} alt='github logo' />
       <img className='expand' onClick={()=>{window.location.href='https://www.linkedin.com/in/anthony-infortunio-872645220'}} src={linkedinLogo} alt='linkedin logo' />
       <img className='expand' onClick={()=>{window.location.href='mailto:contact@anthonyinfortun.io'}} src={mailImg} alt='mailing envelope' />
-      <img className='expand' src={resumeImg} alt='white paper' />
+      <a href={resumePDF}><img className='expand' src={resumeImg} alt='white paper' /></a>
     </div>
   )
 };
