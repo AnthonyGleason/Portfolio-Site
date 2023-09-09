4674ca891ed5a8d21f52501017e823b53b379730
Anthony Infortunio <contact@anthonyinfortun.io>
2023-09-08 22:10:20 -0400

    updates

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,18 +6,17 @@ import passninjadesktop from '../assets/projects/passninjadesktp.png';
 
 export default function Projects() {
   const slideData = [
-    
-    {
-      name: "Where's My Package?",
-      url: 'https://anthonygleason.github.io/Wheres-My-Package/',
-      codeUrl: 'https://github.com/anthonygleason/Wheres-My-Package',
-      desktopIMG: `url(${wmpdesktop})`,
-    },
     {
       name: "Pass Ninja",
       url: 'https://anthonygleason.github.io/Pass-Ninja/',
       codeUrl: 'https://github.com/anthonygleason/Pass-Ninja',
       desktopIMG: `url(${passninjadesktop})`,
+    },
+    {
+      name: "Where's My Package?",
+      url: 'https://anthonygleason.github.io/Wheres-My-Package/',
+      codeUrl: 'https://github.com/anthonygleason/Wheres-My-Package',
+      desktopIMG: `url(${wmpdesktop})`,
     },
     {
       name: 'Newsweek Layout Clone',
@@ -31,31 +30,33 @@ export default function Projects() {
     <section className='projects' id='projects'>
       <h1 data-aos='fade-right' className='projects-title'>My Projects</h1>
       <ul className='projects-slideshow'>
-        {slideData.map((slide, index) => (
-          <li
-          data-aos="fade-right"
-          className='slide-item expand'
-          style={{ backgroundImage: slide.desktopIMG}}
-          key={index}
-          onClick={() => {window.location.href = slide.url}}
-          > 
-            <div className='slide-desc'>
-              <p className='slide-name'>{slide.name}</p>
-              <a className='slide-link' href={slide.url}>
-                Live Demo
-              </a>
-              <a className='slide-link' href={slide.codeUrl}>
-                View Source Code
-              </a>
-            </div>       
-          </li>
-        ))}
+        {
+          slideData.map((slide, index) => (
+            <li
+            data-aos="fade-right"
+            className='slide-item expand'
+            style={{ backgroundImage: slide.desktopIMG}}
+            key={index}
+            onClick={() => {window.location.href = slide.url}}
+            > 
+              <div className='slide-desc'>
+                <p className='slide-name'>{slide.name}</p>
+                <a className='slide-link' href={slide.url}>
+                  Live Demo
+                </a>
+                <a className='slide-link' href={slide.codeUrl}>
+                  View Source Code
+                </a>
+              </div>       
+            </li>
+          ))
+        }
       </ul>
       <button
         data-aos='fade-right'
         className='view-more'
         onClick={() => {
-          window.location.href = 'https://github.com/antinf?tab=repositories';
+          window.location.href = 'https://github.com/anthonygleason?tab=repositories';
         }}
       >
         View More Projects
