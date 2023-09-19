b5429e13e5eef2c4bd6e34959dca53259b643895
Anthony Infortunio <contact@anthonyinfortun.io>
2023-09-19 16:41:55 -0400

    update links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -13,7 +13,7 @@ export default function Footer(){
       <h1 data-aos='fade-right' className='footer-title'>Contact Me</h1>
       <p data-aos='fade-right' className='footer-text'>
         <span>"I look forward to reading your message!"</span>
-        <em className='em' data-aos='fade-right'><a className='mail' href='mailto:contact@anthonyinfortun.io'>contact@anthonyinfortun.io</a></em>
+        <em className='em' data-aos='fade-right'><a className='mail' href='mailto:contact@anthonygleason.com'>contact@anthonygleason.com</a></em>
       </p>
       <ContactButtons />
     </footer>
