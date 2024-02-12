deb494d355fcd7f67ea017f8d42e4e53c692eab4
Anthony Infortunio <contact@anthonygleason.com>
2024-02-12 16:07:07 -0500

    update intro

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,7 +10,7 @@ export default function About() {
       <article className='about-content'>
         <p className='about-text'>
           Hey, I'm Anthony, a Full Stack Developer on a mission to bring your digital dreams to life. No buzzwords, just{' '}
-          <em className='em'>clean code</em> and <em className='em'>killer web applications</em>. I'm all about making 
+          <em className='em'>clean code</em> and <em className='em'>innovative web applications</em>. I'm all about making 
           unique and memorable user experiences. 
         </p>
       </article>
